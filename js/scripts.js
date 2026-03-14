0bb2016e7ae3fb1274802c8bbdab6cacd464e905
unknown <emdelbridge@gmail.com>
2026-03-14 15:27:32 -0500

    claude styling edits for mobile

--- js/scripts.js
+++ js/scripts.js
@@ -120,26 +120,46 @@
 		var $masonry_gallery = jQuery('.masonry-gallery.gallery');
 		if ( $masonry_gallery.length > 0 ) {
 
+			var MOBILE_BREAKPOINT = 900;
+
+			function initIsotope() {
+				if ( false ) {
+					$masonry_gallery.each( function(index, element) {
+						if ( !$(element).data('isotope') ) {
+							$(element).isotope({
+								layoutMode: 'fitRows',
+								itemSelector: '.gallery-item'
+							});
+							$(element).isotope('layout');
+						}
+					});
+				} else {
+					$masonry_gallery.each( function(index, element) {
+						if ( $(element).data('isotope') ) {
+							$(element).isotope('destroy');
+						}
+					});
+				}
+			}
+
+			initIsotope();
+
+			$(window).on('resize', function() {
+				initIsotope();
+			});
+
+			// filtering
+			jQuery('#gallery-filter li a').on('click', function(){
+				jQuery('#gallery-filter li a').removeClass('active');
+				jQuery(this).addClass('active');
+				var selector = jQuery(this).attr('data-filter');
+				$masonry_gallery.isotope({ filter: selector });
+				return false;
+			});
+
+			// changing layout
 			$masonry_gallery.each( function(index, element) {
 				var $masonry_items = $(element).find('.gallery-item');
-			
-				// set masonry layout
-				$(element).isotope({
-					masonry: { columnWidth: $(element).find('.gallery-item')[0] },
-					itemSelector: '.gallery-item'
-				});
-				$(element).isotope('layout');
-					
-				// filtering
-				jQuery('#gallery-filter li a').on('click', function(){
-					jQuery('#gallery-filter li a').removeClass('active');
-					jQuery(this).addClass('active');
-					var selector = jQuery(this).attr('data-filter');
-					$masonry_gallery.isotope({ filter: selector });
-					return false;
-				});
-
-				// changing layout
 				jQuery('#grid-changer li a').on('click', function(){
 					jQuery('#grid-changer li a').removeClass('active');
 					jQuery(this).toggleClass('active');
@@ -150,7 +170,6 @@
 					$masonry_items.toggleClass(jQuery(this).closest('li').attr('class'));
 					$masonry_gallery.isotope('layout');
 				});
-			
 			});
 		}
 
